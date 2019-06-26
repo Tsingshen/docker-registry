6d5af04f982e54cb769df6767fa0e7afac71d5df
Mr. Shen <shenqinpeng@126.com>
2019-06-26 14:23:59 +0800

    Update docekr_registry.sh

--- docekr_registry.sh
+++ docekr_registry.sh
@@ -6,11 +6,28 @@ services:
   registry:
     image: registry:2
     ports:
-      - 80:5000
+      - 8080:5000
     volumes:
-      - /data/registry:/var/lib/registry
+      - /data/registry-registry:/var/lib/registry
     restart: always
     environment:
       REGISTRY_STORAGE_DELETE_ENABLED: "true"
+EOF
+docker-compose up -d
+
+
+## mirror
+cat > docker-compose.yml <<- EOF
+version: "3"
+services:
+  mirror:
+    image: registry:2
+    ports:
+      - 80:5000
+    volumes:
+      - /data/registry-mirror:/var/lib/registry
+    restart: always
+    environment:
+      REGISTRY_PROXY_REMOTEURL: "https://labrtv3.mirror.aliyuncs.com"
 EOF
 docker-compose up -d
